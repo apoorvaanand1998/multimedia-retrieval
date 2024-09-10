62733c458e6603904d6bc68aeed1d7993d1a01ca
Apoorva Anand <apoorvaanand.work@gmail.com>
2024-09-10 14:52:03 +0200

    Show types of faces

--- mr-tool/src/1/analysis.py
+++ mr-tool/src/1/analysis.py
@@ -1,7 +1,7 @@
 from pathlib import PurePath
-from vedo import load, show
+from vedo import load, Mesh
 
-def shape_class(file_path: str, ez: bool=True) -> str:
+def shape_class(file_path: str) -> str:
     return PurePath(file_path).parent.name
 
 def n_o_vertices(file_path: str) -> int:
@@ -12,4 +12,10 @@ def n_o_faces(file_path: str) -> int:
     mesh = load(file_path)
     return mesh.dataset.GetNumberOfCells()
 
-print(n_o_faces("../ShapeDatabase_INFOMR/Bed/D00031.obj"))
+def n_o_triangles_quads(file_path: str) -> tuple[int, int]:
+    mesh = load(file_path)
+    varr = mesh.count_vertices()
+    return (varr[varr == 3].size, varr[varr == 4].size) 
+    ## weird sytax for filtering numpy arrays
+
+print(n_o_triangles_quads("../ShapeDatabase_INFOMR/Bed/D00031.obj"))
