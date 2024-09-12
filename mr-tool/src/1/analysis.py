1e1ffd88a48fd61c4025d1ca50a28158fea62f71
Apoorva Anand <apoorvaanand.work@gmail.com>
2024-09-12 21:07:02 +0200

    Output basic stats to csv

--- mr-tool/src/1/analysis.py
+++ mr-tool/src/1/analysis.py
@@ -2,18 +2,37 @@ from pathlib import PurePath, Path
 from vedo import load, Mesh, show
 import numpy as np
 import csv
+from typing import TypeVar
 
-def output_csv(shape_path: str, csv_path: str, ez_mode: bool=True):
+def output_csv(shape_path: str, csv_path: str, ez_mode: bool=True) -> int:
+    objs = concat(list_objs(shape_path, ez_mode=True))
+    
+    with open(csv_path, mode='w') as stat_file:
+        stat_writer = csv.writer(stat_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        for obj in objs:
+            n  = str(obj)
+            c  = shape_class(n)
+            v  = n_o_vertices(n)
+            f  = n_o_faces(n)
+            tq = n_o_triangles_quads(n)
+            bb = bounding_box(n).tolist()
+
+            stat_writer.writerow([n, c, v, f, tq, bb])
+    
+    return len(objs)
+
+def list_objs(shape_path: str, ez_mode: bool=True) -> list[list[Path]]:
     p    = Path(shape_path)
     dirs = [d for d in p.iterdir() if d.is_dir()]
     objg = [d.iterdir() for d in dirs]
     objs = [list(g) for g in objg]
+
     if ez_mode: 
         objs = [o[0:3] for o in objs]
+
     return objs
-    with open(csv_path, mode='w') as cfile:
-        pass
-    
+
 def shape_class(file_path: str) -> str:
     return PurePath(file_path).parent.name
 
@@ -37,6 +56,12 @@ def bounding_box(file_path: str, show_box: bool=False) -> np.ndarray:
     if show_box: 
         show(mesh, b)
         print(b)
-    return b.bounds() ## ((min x, max x), (min y, max y), (min z, max z))
+    return b.bounds() ## [min x, max x, min y, max y, min z, max z]
 
-print(output_csv("../ShapeDatabase_INFOMR", "", True))
+T = TypeVar('T')
+def concat(xss: list[list[T]]) -> list[T]: 
+    ## aka flatten
+    return [x for xs in xss for x in xs]
+
+
+print(output_csv("../ShapeDatabase_INFOMR", "../Output/stats.csv"))
