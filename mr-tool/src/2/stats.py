82dd179d8a69ff4bf06002ac1b3c03fa42eceb67
Apoorva Anand <apoorvaanand.work@gmail.com>
2024-09-13 16:55:36 +0200

    All faces are triangles

--- mr-tool/src/2/stats.py
+++ mr-tool/src/2/stats.py
@@ -1,2 +1,15 @@
-def check_all_faces_are_triangles(shape_path: str, csv_path: str) -> bool:
-    pass
+import csv 
+
+## 2.2
+def check_all_faces_are_triangles(csv_path: str) -> bool:
+    with open(csv_path) as stat_file:
+        stat_reader = csv.reader(stat_file, delimiter=',', quotechar='"')
+        for row in stat_reader:
+            face_count = int(row[3])
+            tri_count  = eval(row[4])[0]
+            if face_count != tri_count:
+                print(row)
+                return False
+    return True
+
+print(check_all_faces_are_triangles("../Output/stats.csv"))
