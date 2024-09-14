aead57d4fc6bbe55bcd34f00b921e53f2925592b
Apoorva Anand <apoorvaanand.work@gmail.com>
2024-09-14 13:27:48 +0200

    Average Shape (2.2a)

--- mr-tool/src/2/stats.py
+++ mr-tool/src/2/stats.py
@@ -1,6 +1,18 @@
 import csv 
 
 ## 2.2
+
+def avg_shape(csv_path: str) -> tuple[int, int]:
+    """(average vertex count, average face count)"""
+    n, total_vc, total_fc = 0, 0, 0
+    with open(csv_path) as stat_file:
+        stat_reader = csv.reader(stat_file, delimiter=',', quotechar='"')
+        for row in stat_reader:
+            total_vc += int(row[2])
+            total_fc += int(row[3])
+            n        += 1
+    return (total_vc/n, total_fc/n)
+
 def check_all_faces_are_triangles(csv_path: str) -> bool:
     with open(csv_path) as stat_file:
         stat_reader = csv.reader(stat_file, delimiter=',', quotechar='"')
@@ -12,4 +24,4 @@ def check_all_faces_are_triangles(csv_path: str) -> bool:
                 return False
     return True
 
-print(check_all_faces_are_triangles("../Output/stats.csv"))
+print(avg_shape("../Output/stats.csv"))
