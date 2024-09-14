91be5e1f15ca123ce481edbce793199b3aa2f7e5
Apoorva Anand <apoorvaanand.work@gmail.com>
2024-09-14 14:02:41 +0200

    Show min, max vertex and face counts

--- mr-tool/src/2/stats.py
+++ mr-tool/src/2/stats.py
@@ -1,6 +1,24 @@
 import csv 
-
+import math
 ## 2.2
+
+def min_max(csv_path: str) -> tuple[tuple[int, int], tuple[int, int]]:
+    """((min vc, max vc), (min fc, max fc))"""
+    max_vc, max_fc = 0, 0
+    min_vc, min_fc = math.inf, math.inf
+
+    minvc, maxvc, minfc, maxfc = "", "", "", ""
+
+    with open(csv_path) as stat_file:
+        stat_reader = csv.reader(stat_file, delimiter=',', quotechar='"')
+        for row in stat_reader:
+            if int(row[2]) > max_vc: max_vc, maxvc = float(row[2]), row[0]
+            if int(row[2]) < min_vc: min_vc, minvc = float(row[2]), row[0]
+            if int(row[3]) > max_fc: max_fc, maxfc = float(row[3]), row[0]
+            if int(row[3]) < min_fc: min_fc, minfc = float(row[3]), row[0]
+
+    print(minvc, maxvc, minfc, maxfc)
+    return ((min_vc, max_vc), (min_fc, max_fc))
 
 def avg_shape(csv_path: str) -> tuple[int, int]:
     """(average vertex count, average face count)"""
@@ -24,4 +42,4 @@ def check_all_faces_are_triangles(csv_path: str) -> bool:
                 return False
     return True
 
-print(avg_shape("../Output/stats.csv"))
+print(min_max("../Output/stats.csv"))
