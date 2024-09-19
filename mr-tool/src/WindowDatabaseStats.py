1ea94c0951602a994b54b5431e6abe2ca9a83745
Vlad Puscaru <vlad.cpuscaru@gmail.com>
2024-09-19 16:06:34 +0200

    Show average shapes

--- mr-tool/src/WindowDatabaseStats.py
+++ mr-tool/src/WindowDatabaseStats.py
@@ -48,7 +48,8 @@ class WindowDatabaseStats(QMainWindow):
             db_map, db_count = utils.get_database_map(db_path)
             stats_file_path = str(os.path.join(constants.OUTPUT_DIR_RELATIVE_PATH, db_name, constants.STATS_FILE_NAME))
             if os.path.exists(stats_file_path):
-                self._ui_db_widget = WidgetStats(db_map, db_count, db_name)
+                db_stats = utils.get_output_stats(db_name)
+                self._ui_db_widget = WidgetStats(db_map, db_count, db_name, db_stats)
             else:
                 self._ui_db_widget = WidgetEmptyStats(db_map, db_count, db_name)
         else:
