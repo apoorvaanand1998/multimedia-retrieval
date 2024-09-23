f278f274c5f6375cf244252b2d2c74f38f4eec2b
Apoorva Anand <apoorvaanand.work@gmail.com>
2024-09-23 18:06:57 +0200

    Resample

--- mr-tool/src/StepTwo/s3/resample.py
+++ mr-tool/src/StepTwo/s3/resample.py
@@ -1,5 +1,6 @@
 import csv
 import vedo as vd
+import math
 
 def subsample(csv_path: str, output_path: str) -> list[int]:
     lines_changed = []
@@ -9,29 +10,47 @@ def subsample(csv_path: str, output_path: str) -> list[int]:
             cs_reader = csv.reader(curr_stat_file, delimiter=',', quotechar='"')
             ns_writer = csv.writer(new_stat_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for row in cs_reader:
-                l += 1
-                v  = int(row[2])
-                f  = int(row[3])
-                m  = vd.load(row[0])
+                v = int(row[2])
+                f = int(row[3])
+                m = vd.load(row[0])
                 
-                if not cond(v, f):
+                while v <= 4000 or v >= 6000:
+                    m_copy = m.copy()
                     lines_changed.append(l)
-                    i = 0
-                    while not cond(v, f):
-                        print("i%2", i%2, fs(m)[i%2][0])
-                        m = fs(m)[i%2][1]
+                    if v >= 6000:
+                        m.decimate(n=5000)
+                        v = m.dataset.GetNumberOfPoints()
+                    elif v <= 4000:
+                        m1 = m.copy()
+                        m2 = m.copy()
+                        m3 = m.copy()
+                        m4 = m.copy()
+                        m.subdivide()
+                        m1.subdivide()
+                        m2.subdivide()
+                        m3.subdivide()
+                        m4.subdivide()
+
+                        vs = [(m, m.dataset.GetNumberOfPoints()),
+                              (m1, m1.dataset.GetNumberOfPoints()),
+                              (m2, m2.dataset.GetNumberOfPoints()),
+                              (m3, m3.dataset.GetNumberOfPoints()),
+                              (m4, m4.dataset.GetNumberOfPoints())]
+                        print(vs)
+                        res  = max(vs, key=lambda x : x[1])
+                        m, v = res
+                    if v == 0:
+                        m = m_copy
                         v = m.dataset.GetNumberOfPoints()
                         f = m.dataset.GetNumberOfCells()
-                        print("New", m)
-                        i += 1
+                        print(m)
+                        vd.show(m)
+                        break
+                    f = m.dataset.GetNumberOfPoints()
+                    print(m)
                     vd.show(m)
-                ns_writer.writerow([row[0], v, f])
+                ns_writer.writerow([row[0], m, v, f])
+                l += 1
             return lines_changed
-
-def cond(v, f):
-    return (v >= 4000 and v <= 6000) or (f >= 9000 and f <= 11000)
-
-def fs(m):
-    return [("sub", vd.Mesh.subdivide(m, 10)), ("dec", vd.Mesh.decimate(m, 0.1))]
 
 subsample("../Output/stats.csv", "../Output/resampled.csv")        
