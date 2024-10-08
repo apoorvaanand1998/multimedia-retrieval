e9efe26d53d54e77170f2b5088e7198d515d557b
Nick Dijkhuizen <55748715+ND-code-ai@users.noreply.github.com>
2024-10-08 13:13:18 +0200

    (WIP): currently diameter calculation working

--- get_global_descriptors.py
+++ get_global_descriptors.py
@@ -3,8 +3,9 @@ import vedo as vd
 import numpy as np
 from resample_open3d import get_file_paths
 import trimesh as tm
+import time
 
-def get_global_descriptors(mesh: o3d.geometry.TriangleMesh, descriptor: str) -> list[float] | None:
+def get_global_descriptors(mesh: o3d.geometry.TriangleMesh, descriptor: str) -> float | None:
     convex_hull = mesh.compute_convex_hull()[0]
     if descriptor == 'surface_area':
         return mesh.get_surface_area()
@@ -24,7 +25,6 @@ def get_global_descriptors(mesh: o3d.geometry.TriangleMesh, descriptor: str) -> 
                 distance = np.linalg.norm(hull_vertices[i] - hull_vertices[j])
                 if distance > max_distance:
                     max_distance = distance
-
         return max_distance
 
 def calculate_compactness(mesh: o3d.geometry.TriangleMesh) -> float:
@@ -32,17 +32,17 @@ def calculate_compactness(mesh: o3d.geometry.TriangleMesh) -> float:
     print(triangles)
 
 if __name__ == "__main__":
-    source_directory = "ShapeDB_sample"
+    source_directory = "remeshed_ShapeDB"
     destination_directory = "ShapeDB_sample_global_descriptors"
     files, destination_files = get_file_paths(source_directory, destination_directory)
     for file in files[:1]:
         print(f"Processing: {file}")
         o3d_mesh = o3d.io.read_triangle_mesh(file)
-        surface_area = get_global_descriptors(o3d_mesh, 'surface_area')
-        compactness = get_global_descriptors(o3d_mesh, 'compactness')
-        rectangularity = get_global_descriptors(o3d_mesh, 'rectangularity')
+        #surface_area = get_global_descriptors(o3d_mesh, 'surface_area')
+        #compactness = get_global_descriptors(o3d_mesh, 'compactness')
+        #rectangularity = get_global_descriptors(o3d_mesh, 'rectangularity')
         diameter = get_global_descriptors(o3d_mesh, 'diameter')
-        print(f"Surface Area: {surface_area}")
-        print(f"Compactness: {compactness}")
-        print(f"Rectangularity: {rectangularity}")
+        #print(f"Surface Area: {surface_area}")
+        #print(f"Compactness: {compactness}")
+        p#rint(f"Rectangularity: {rectangularity}")
         print(f"Diameter: {diameter}")
