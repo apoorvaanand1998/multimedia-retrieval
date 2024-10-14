f6ac8f3dc82956801b22b4f5251eb6f10a78770e
Nick Dijkhuizen <55748715+ND-code-ai@users.noreply.github.com>
2024-10-14 15:50:34 +0200

    volume calculation formula added

--- get_global_descriptors.py
+++ get_global_descriptors.py
@@ -3,6 +3,26 @@ import numpy as np
 from resample_open3d import get_file_paths
 
 
+def calculate_volume(vertices: np.ndarray, triangles: np.ndarray):
+    total_volume = 0
+    origin = np.array([0.0, 0.0, 0.0])
+
+    for tri in triangles:
+        x1 = vertices[tri[0]]
+        x2 = vertices[tri[1]]
+        x3 = vertices[tri[2]]
+
+        v1 = x1 - origin
+        v2 = x2 - origin
+        v3 = x3 - origin
+
+        cross_product = np.cross(v1, v2)
+        volume_tetrahedron = np.dot(cross_product, v3)
+
+        total_volume += volume_tetrahedron
+
+    return abs(total_volume) / 6
+
 
 def calculate_compactness(surface_area: float, shape_volume: float) -> float:
     return surface_area ** 3 / shape_volume ** 2
@@ -18,8 +38,8 @@ def calculate_diameter(hull_vertices: np.ndarray) -> float:
     return max_distance
 
 
-def get_global_descriptors(mesh: o3d.geometry.TriangleMesh) -> dict:
-    shape_volume = mesh.get_volume()
+def get_global_descriptors(mesh: o3d.geometry.TriangleMesh) -> dict[str, float]:
+    shape_volume = calculate_volume(np.asarray(mesh.vertices), np.asarray(mesh.triangles))
     surface_area = mesh.get_surface_area()
     convex_hull = mesh.compute_convex_hull()[0]
     hull_volume = convex_hull.get_volume()
@@ -43,10 +63,11 @@ def get_global_descriptors(mesh: o3d.geometry.TriangleMesh) -> dict:
 if __name__ == "__main__":
     source_directory = "remeshed_ShapeDB"
     destination_directory = "ShapeDB_sample_global_descriptors"
-    o3d_sphere = o3d.geometry.TriangleMesh.create_sphere()
 
-    global_descriptors = get_global_descriptors(o3d_sphere)
-    print(global_descriptors)
+    # EXAMPLE USAGE
+    o3d_sphere = o3d.geometry.TriangleMesh.create_sphere()
+    global_descriptor_sphere = get_global_descriptors(o3d_sphere)
+
     o3d_box = o3d.geometry.TriangleMesh.create_box(width=2)
-    global_descriptors = get_global_descriptors(o3d_box)
-    print(global_descriptors)
+    global_descriptor_box = get_global_descriptors(o3d_box)
+
