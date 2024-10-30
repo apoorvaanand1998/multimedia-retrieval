dff4097b3ff6a291e3192f7b1586ed16aef7b0d8
Apoorva Anand <apoorvaanand.work@gmail.com>
2024-10-30 11:38:01 +0100

    Starting New Gen again. Did New Gen on previous db before. FML

--- mr-tool/src/StepThree/s2b/hist_gen.py
+++ mr-tool/src/StepThree/s2b/hist_gen.py
@@ -9,12 +9,14 @@ import pathlib as p
 import csv
 import numpy as np
 
+shape_db_path = '../../FINAL_remeshed_repaired_normalized_ShapeDB'
+
 def write_hists(cls: str, mthd: str, dst: str):
     """ Write histogram data from class cls,
         using mthd descriptor,
         into destination dst """
 
-    from_path = p.Path('../../remeshed_normalized_filled_ShapeDB/' + cls)
+    from_path = p.Path(shape_db_path + cls)
     objs      = [x for x in from_path.iterdir()]
     strd_objs = [str(o) for o in objs]
     loaded    = [vedo.load(o) for o in strd_objs]
@@ -36,7 +38,7 @@ def write_hists(cls: str, mthd: str, dst: str):
             res = []
     plt.title(mthd + ' for ' + cls)
     plt.savefig(dst+mthd+'_'+cls+'.png')
-    plt.show()
+    ##plt.show()
 
     sob = [(mthd, s) for s in strd_objs]
     res = zip(sob, res)
@@ -44,9 +46,9 @@ def write_hists(cls: str, mthd: str, dst: str):
         sdw = csv.writer(sd, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         _   = [sdw.writerow(r) for r in res]
 
-c_vlad = ['Wheel', 'WheelChair']
-
+cs = p.Path(shape_db_path)
+cs = [c.name for c in cs.iterdir()]
 np.set_printoptions(threshold=np.inf)
-for c in c_vlad:
+for c in cs:
     for m in ['a3', 'd1', 'd2', 'd3', 'd4']:
-        write_hists(c, m, '../../Output/ShapePropDesc/')
+        write_hists(c, m, '../../Output/ShapePropDesc2/')
