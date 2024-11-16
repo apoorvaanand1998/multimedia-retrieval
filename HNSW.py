1df22c2b877aae2a7add0338486ace598407c518
Nick Dijkhuizen <55748715+ND-code-ai@users.noreply.github.com>
2024-11-16 23:26:06 +0100

    Update scaling part

--- HNSW.py
+++ HNSW.py
@@ -1,22 +1,23 @@
 import hnswlib
 import numpy as np
+import pandas as pd
 
 
 def build_hnsw_graph(feature_vectors: np.ndarray) -> hnswlib.Index:
 
-    assert feature_vectors.dtype == 'f'
+    assert feature_vectors.dtype == 'float64'
 
     dim = feature_vectors.shape[1]
     hnsw_index = hnswlib.Index(space='l2', dim=dim)
 
-    hnsw_index.init_index(max_elements=feature_vectors.shape[0], ef_construction=300, M=32)
+    hnsw_index.init_index(max_elements=feature_vectors.shape[0], ef_construction=500, M=64)
     hnsw_index.add_items(feature_vectors)
 
     return hnsw_index
 
 
 def query_hnsw(index: hnswlib.Index, query_vector: np.ndarray, k: int) -> tuple[np.ndarray, np.ndarray]:
-    assert query_vector.shape[1] == index.dim
+    assert query_vector.shape[0] == index.dim
 
     indices, dist = index.knn_query(query_vector, k=k)
     unpacked_indices, unpacked_dist = indices[0], dist[0]
@@ -25,19 +26,17 @@ def query_hnsw(index: hnswlib.Index, query_vector: np.ndarray, k: int) -> tuple[
 
 
 if __name__ == '__main__':
-    toy_data = np.random.random((100, 1000)).astype('float32')
-    # make sure input data doesnt contain the paths!
-    hnsw = build_hnsw_graph(toy_data)
+    data = pd.read_csv('USE_THIS_MATRIX.csv')
+    path_data = data['name'].values
+    data_numeric: np.ndarray = data.drop(columns=['name', 'id']).values
+    hnsw = build_hnsw_graph(data_numeric)
 
-    toy_query = np.random.random((1, 1000)).astype('float32')
+    toy_query = data_numeric[70]
     neighbor_indices, distances = query_hnsw(hnsw, toy_query, 5)
 
-    resulting_shapes = toy_data[neighbor_indices] # still index to path of shape
+    resulting_shapes = path_data[neighbor_indices]
 
-    index_for_shape_paths = 0 # assuming first column vector is the paths
-    resulting_shape_paths = resulting_shapes[:, 0]
-
-    path_and_distances_dict = {path: distance for path, distance in zip(resulting_shape_paths, distances)}
+    path_and_distances_dict = {path: distance for path, distance in zip(resulting_shapes, distances)}
     print(path_and_distances_dict)
 
 
